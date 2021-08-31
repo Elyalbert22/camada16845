db04251993b101cfaac98be230d860a43284efbe
Elyalbert22 <elizabethalbert22@gmail.com>
2021-08-31 20:30:06 -0300

    clase 1

--- clase1/clase1.js
+++ clase1/clase1.js
@@ -3,7 +3,7 @@ let nombre = prompt("Ingresa el nombre de tu Emprendimiento");
 console.log("Tu Emprendimiento se llama:", nombre);
 
 let numero1 = parseInt(prompt("Ingresa un número"));
-let numero2 = 20;
+let numero2 = 100;
 const resultado = numero1 + numero2;
 
 console.log("Son la cantidad de visitas que tendras en tu sitio: " + resultado)
