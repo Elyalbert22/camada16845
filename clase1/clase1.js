b8b1d191d208af682a9dc9c50c049d601ae230bf
Elyalbert22 <elizabethalbert22@gmail.com>
2021-08-31 20:27:16 -0300

    desafio clase 1

---
+++ clase1/clase1.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Ingresa el nombre de tu Emprendimiento");
+
+console.log("Tu Emprendimiento se llama:", nombre);
+
+let numero1 = parseInt(prompt("Ingresa un número"));
+let numero2 = 20;
+const resultado = numero1 + numero2;
+
+console.log("Son la cantidad de visitas que tendras en tu sitio: " + resultado)
