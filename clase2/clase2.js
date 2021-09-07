8f33921ed7156b20a6669f1e62b89a6f8ee694b4
Elyalbert22 <elizabethalbert22@gmail.com>
2021-09-07 10:31:23 -0300

    desafio2

---
+++ clase2/clase2.js
@@ -0,0 +1,7 @@
+let recomendarPlato = prompt("Hola! ¿Sos vegano?")
+
+if(recomendarPlato === "si"){
+    console.log("Te recomiendo los Fideos de arroz con salteado de tofu y pimiento.")
+} else{
+    console.log ("Te recomiendo el Arrollado de carne a la suiza.")
+}
