a25164b14188fb04bc0667929ba3c604dd15c9cc
Elyalbert22 <elizabethalbert22@gmail.com>
2021-09-15 10:51:23 -0300

    cambios en el codigo clase 4

--- clase4/clase4.js
+++ clase4/clase4.js
@@ -1,15 +1,19 @@
 let corteDeCarne = prompt("¿Que corte de carne desea?: \nNalga \nPicada \nLomo \n\nCombinalos como quieras.");
+let gramos = parseInt(prompt("Ingrese el peso deseado en Gramos."));
+let precioNalga = 650;
+let precioPicada = 760;
+let precioLomo = 460;
 
 // do {
     switch (corteDeCarne) {
         case "nalga":
-            console.log(calcularPrecio());
+            console.log(precioPorKilo(gramos, precioNalga));
             break;
         case "picada":
-            console.log(calcularPrecio());
+            console.log(precioPorKilo(gramos, precioPicada));
             break;
         case "lomo":
-            console.log(calcularPrecio());
+            console.log(precioPorKilo(gramos, precioLomo));
             break;
         case "nalga y picada":
             console.log(calcularDosCortes());
@@ -33,10 +37,9 @@ let corteDeCarne = prompt("¿Que corte de carne desea?: \nNalga \nPicada \nLomo 
 // } while (corteDeCarne == "");
 
 
-function calcularPrecio() {
-    let gramos = parseInt(prompt("Ingrese el peso deseado en Gramos."));
-    let costo = parseInt(prompt("Ingrese precio por Kilo del Corte."));
-    let precioCorte = (gramos * costo) / 1000;
+
+function precioPorKilo(gramos, precio){
+    let precioCorte = (gramos * precio) / 1000;
     return Math.round(precioCorte);
 }
 
@@ -45,9 +48,7 @@ function calcularDosCortes() {
     let precio1 = parseInt(prompt("Ingrese precio por Kilo del 1° Corte."));
     let gramos2 = parseInt(prompt("Ingrese el peso deseado en Gramos del 2° Corte."));
     let precio2 = parseInt(prompt("Ingrese precio por Kilo del 2° Corte."));
-    const precioCorte1 = (gramos1 * precio1) / 1000;
-    const precioCorte2 = (gramos2 * precio2) / 1000;
-    const precioTotal1 = precioCorte1 + precioCorte2;
+    const precioTotal1 = precioPorKilo(gramos1, precio1) + precioPorKilo(gramos2, precio2);
     return Math.round(precioTotal1);
 }
 
@@ -58,9 +59,6 @@ function calcularTresCortes() {
     let precio2 = parseInt(prompt("Ingrese precio por Kilo del 2° Corte."));
     let gramos3 = parseInt(prompt("Ingrese el peso deseado en Gramos del 3° Corte."));
     let precio3 = parseInt(prompt("Ingrese precio por Kilo del 3° Corte."));
-    const precioCorte1 = (gramos1 * precio1) / 1000;
-    const precioCorte2 = (gramos2 * precio2) / 1000;
-    const precioCorte3 = (gramos3 * precio3) / 1000;
-    const precioTotal2 = precioCorte1 + precioCorte2 + precioCorte3;
+    const precioTotal2 = precioPorKilo(gramos1, precio1) + precioPorKilo(gramos2, precio2) + precioPorKilo(gramos3, precio3);
     return Math.round(precioTotal2);
 }
